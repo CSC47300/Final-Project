37969d839d25f170a56e5ed87c8ee96ccaff56b8
apparatusjazz <maui.arcuri@gmail.com>
2020-10-15 12:04:42 -0400

    Added required nav bar items

--- webpage/src/Components/nav-bar.js
+++ webpage/src/Components/nav-bar.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 
 class NavBar extends Component {
-    constructor() {
-
+    constructor(props) {
+        super(props);
     }
 
     render() {
         return (
-            <Navbar>
-
-            </Navbar>
+            <div>
+                <Navbar bg="dark" variant="dark" expand="md">
+                    <Navbar.Brand href="#home">Newercloud</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse className="justify-content-end">
+                        <Nav>
+                            <Nav.Link href="#upload">Upload</Nav.Link>
+                            <NavDropdown title={this.props.userName} alignRight>
+                                <NavDropdown.Item href="#profile">Profile</NavDropdown.Item>
+                                <NavDropdown.Item href="#settings">Settings</NavDropdown.Item>
+                                <NavDropdown.Item href="#logout">Logout</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </div>
         )
     }
 }
