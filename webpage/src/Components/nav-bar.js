44fde762d5acb3d4b9d08200c027ce578fd44d42
apparatusjazz <maui.arcuri@gmail.com>
2020-10-15 10:55:29 -0400

    Added NavBar component

---
+++ webpage/src/Components/nav-bar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Navbar } from 'react-bootstrap';
+
+class NavBar extends Component {
+    constructor() {
+
+    }
+
+    render() {
+        return (
+            <Navbar>
+
+            </Navbar>
+        )
+    }
+}
+
+export default NavBar;
