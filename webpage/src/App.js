0bfe6d3f269837c7f3fca21eff277016f766b361
apparatusjazz <maui.arcuri@gmail.com>
2020-10-19 10:30:40 -0400

    Added track component to test

--- webpage/src/App.js
+++ webpage/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/navigation';
+import Track from './Components/track';
 
 function App() {
   return (
     <div className="App">
       <NavBar userName="Maui A" />
+      <Track playing={false} />
     </div>
   );
 }
