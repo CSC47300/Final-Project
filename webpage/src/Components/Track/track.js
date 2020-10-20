8517f4693db30db9ed3ec27ec95f0761adbf46af
apparatusjazz <maui.arcuri@gmail.com>
2020-10-20 12:16:28 -0400

    Finished basic front end and variable data with props

--- webpage/src/Components/Track/track.js
+++ webpage/src/Components/Track/track.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import { BsHeart, BsArrowRepeat } from 'react-icons/bs';
+import { BsHeart, BsArrowRepeat, BsPlay, BsChatSquare } from 'react-icons/bs';
 import "./track.css";
 
 class Track extends Component {
@@ -8,33 +8,41 @@ class Track extends Component {
 
     render() {
         const playing = this.props.playing ? "play-btn pause" : "play-btn play";
+        const post = this.props.userName === this.props.artistName ? "posted" : "reposted";
         return (
             <Container className="track">
                 <Row className="poster">
                     <Col className="">
                         <img className="user-avatar" src="https://i.imgur.com/p3vccAp.jpeg" alt="poster-avatar" />
-                        User posted a track 15 hours ago
+                        &nbsp;{this.props.userName} {post} a track 15 hours ago
                     </Col>
 
                 </Row>
                 <Row className="">
                     <Col lg={2} className="">
-                        <img className="track-art" src="https://i.imgur.com/p3vccAp.jpeg" alt="track-art" />
+                        <img className="track-art" src={this.props.albumArt} alt="track-art" />
                         <button className={playing}></button>
                     </Col>
                     <Col className="">
-                        <Row>Artist name</Row>
-                        <Row>Song name</Row>
+                        <Row><div>{this.props.artistName}</div></Row>
+                        <Row>{this.props.songName}</Row>
                         <Row className="audio-wave"></Row>
                         <Row className="social">
-                            <Col md={1}><button>
-                                <BsHeart />
+                            <Col md={1}><button className="social-icon">
+                                <BsHeart /> {this.props.likes}
                             </button></Col>
-                            <Col md={1}><button>
-                                <BsArrowRepeat />
+                            <Col md={1}><button className="social-icon">
+                                <BsArrowRepeat /> {this.props.reposts}
                             </button></Col>
                             <Col md={3}></Col>
-                            <Col md={2}>89 plays</Col>
+                            <Col md={1}>
+                                <div className="social-icon"><BsPlay /> {this.props.playCount}</div>
+                            </Col>
+                            <Col md={1}>
+                                <div className="social-icon">
+                                    <BsChatSquare /> {this.props.commentCount}
+                                </div>
+                            </Col>
                         </Row>
                     </Col>
                 </Row>
