e51f73798d1559ddb154d277775ed907845f2d2a
apparatusjazz <maui.arcuri@gmail.com>
2020-10-20 22:20:08 -0400

    Adjusted play button, added links for song, artist, user

--- webpage/src/Components/Track/track.js
+++ webpage/src/Components/Track/track.js
@@ -1,31 +1,37 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import { BsHeart, BsArrowRepeat, BsPlay, BsChatSquare } from 'react-icons/bs';
+import { BsHeart, BsArrowRepeat, BsPlay, BsChatSquare, BsPlayFill, BsPauseFill } from 'react-icons/bs';
 import "./track.css";
 
 class Track extends Component {
 
 
     render() {
-        const playing = this.props.playing ? "play-btn pause" : "play-btn play";
+        const playBtn = !this.props.playing ? <BsPlayFill /> : <BsPauseFill className="pause-btn" />;
         const post = this.props.userName === this.props.artistName ? "posted" : "reposted";
         return (
             <Container className="track">
                 <Row className="poster">
                     <Col className="">
                         <img className="user-avatar" src="https://i.imgur.com/p3vccAp.jpeg" alt="poster-avatar" />
-                        &nbsp;{this.props.userName} {post} a track 15 hours ago
+                        &nbsp;<a href={`#${this.props.userName}`}>{this.props.userName}</a> {post} a track 15 hours ago
                     </Col>
 
                 </Row>
                 <Row className="">
                     <Col lg={2} className="">
                         <img className="track-art" src={this.props.albumArt} alt="track-art" />
-                        <button className={playing}></button>
+                        <button className="play-btn">
+                            {playBtn}
+                        </button>
                     </Col>
                     <Col className="">
-                        <Row><div>{this.props.artistName}</div></Row>
-                        <Row>{this.props.songName}</Row>
+                        <Row><div>
+                            <a href={`#${this.props.artistName}`}>{this.props.artistName}</a>
+                        </div></Row>
+                        <Row><div>
+                            <a href={`#${this.props.songName}`}>{this.props.songName}</a>
+                        </div></Row>
                         <Row className="audio-wave"></Row>
                         <Row className="social">
                             <Col md={1}><button className="social-icon">
@@ -36,10 +42,10 @@ class Track extends Component {
                             </button></Col>
                             <Col md={3}></Col>
                             <Col md={1}>
-                                <div className="social-icon"><BsPlay /> {this.props.playCount}</div>
+                                <div className="social-tag"><BsPlay /> {this.props.playCount}</div>
                             </Col>
                             <Col md={1}>
-                                <div className="social-icon">
+                                <div className="social-tag">
                                     <BsChatSquare /> {this.props.commentCount}
                                 </div>
                             </Col>
