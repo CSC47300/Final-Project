dba21a03f0a44bec1c1aa83ea855dcbc98853d71
apparatusjazz <maui.arcuri@gmail.com>
2020-10-22 20:42:29 -0400

    Added waveform visualizer

--- webpage/src/Components/Track/track.js
+++ webpage/src/Components/Track/track.js
@@ -2,9 +2,39 @@ import React, { Component } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { BsHeart, BsArrowRepeat, BsPlay, BsChatSquare, BsPlayFill, BsPauseFill } from 'react-icons/bs';
 import "./track.css";
+import WaveSurfer from 'wavesurfer.js';
 
 class Track extends Component {
 
+    handlePlay = () => {
+        // this.props.togglePlay(); // TODO: implement playing state change in parent
+        this.waveform.playPause();
+    };
+
+    componentDidMount() {
+        const track = this.props.track;
+        let wave = document.querySelector(".waveform");
+        wave.id = this.props.id;
+        wave.classList.remove("waveform");
+        this.waveform = WaveSurfer.create({
+            barWidth: 2,
+            cursorWidth: 1,
+            container: document.getElementById(this.props.id),
+            backend: 'WebAudio',
+            height: 70,
+            progressColor: '#2D5BFF',
+            responsive: true,
+            waveColor: '#EFEFEF',
+            cursorColor: '#2D5BFF',
+            normalize: true
+        });
+        this.waveform.load(track);
+        this.waveform.on("seek", () => {
+            if (this.props.playing)
+                this.waveform.play();
+        })
+
+    }
 
     render() {
         const playBtn = !this.props.playing ? <BsPlayFill /> : <BsPauseFill className="pause-btn" />;
@@ -20,9 +50,9 @@ class Track extends Component {
 
                 </Row>
                 <Row className="main-track">
-                    <Col lg={2} className="art-clm">
+                    <Col lg={3} className="art-clm">
                         <img className="track-art" src={this.props.albumArt} alt="track-art" />
-                        <button className="play-btn">
+                        <button className="play-btn" onClick={this.handlePlay}>
                             {playBtn}
                         </button>
                     </Col>
@@ -33,7 +63,10 @@ class Track extends Component {
                         <Row><div>
                             <a className="link song" href={`#${this.props.songName}`}>{this.props.songName}</a>
                         </div></Row>
-                        <Row className="audio-wave"></Row>
+                        <Row className="">
+                            <Col className="waveform wave-container" md={9}>
+                            </Col>
+                        </Row>
                         <Row className="social">
                             <div ><button className="social-icon like">
                                 <BsHeart /> {this.props.likes}
@@ -41,7 +74,8 @@ class Track extends Component {
                             <div ><button className="social-icon repost">
                                 <BsArrowRepeat /> {this.props.reposts}
                             </button></div>
-                            <Col md={3}></Col>
+                            <Col md={6}>
+                            </Col>
                             <div className="social-tag"><BsPlay /> {this.props.playCount}</div>
                             <div className="social-tag">
                                 <BsChatSquare /> {this.props.commentCount}
