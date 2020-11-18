0df462d3f75fb45ef1809b1f70e2645e54142d3a
apparatusjazz <maui.arcuri@gmail.com>
2020-11-17 23:31:06 -0500

    Added test for getElapsedTime from Helpers

--- webpage/src/App.test.js
+++ webpage/src/App.test.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { getElapsedTime } from './Helpers/helpers';
 
 // test('renders learn react link', () => {
 //   const { getByText } = render(<App />);
 //   const linkElement = getByText(/ /i);
 //   expect(linkElement).toBeInTheDocument();
 // });
-test('sample', () => {
-
+test('get elapsed time in string format', () => {
+    expect(getElapsedTime(Date.now())).toBe("0 seconds ago");
+    expect(getElapsedTime(Date.now() - 10000)).toBe("10 seconds ago");
+    expect(getElapsedTime(Date.now() - 70000)).toBe("1 minute ago");
 });
