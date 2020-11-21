acf0fc4a7530b94331c04e8cb6e2ac7898ec1b56
apparatusjazz <maui.arcuri@gmail.com>
2020-11-20 23:44:19 -0500

    Finished function to get posts

--- webpage/src/Components/Feed/feed.js
+++ webpage/src/Components/Feed/feed.js
@@ -1,10 +1,12 @@
-import React, { useRef, useContext, useState, useEffect } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { UserContext } from '../../Providers/UserProvider.js';
 import { db } from '../../firebase';
 import { createTrack, getElapsedTime } from '../../Helpers/helpers';
-import firebase from "firebase/app";
 
 const Feed = (props) => {
+
+    const [tracks, setTracks] = useState([]);
+
     const user = useContext(UserContext);
 
     const getPosts = () => {
@@ -16,21 +18,58 @@ const Feed = (props) => {
             following.forEach(artist => {
                 requests.push(db.collection('users').doc(artist).get());
             });
-            return Promise.all(requests);
+            return Promise.all(requests);           // Get all users followed
         }).then(docs => {
             for (let i = 0; i < data.length; i++) {
                 let data = docs[i].data();
+                let newPosts = data.posts;
+                newPosts.forEach(post => {          // Get all posts by user and add the userName to the post object
+                    post["postedBy"] = data.displayName;
+                })
                 posts.concat(data.posts);
             }
+            posts.sort((a, b) => b.postDate - a.postDate);  // Sort by latest posts first
+            requests = [];
+            posts.forEach(post => {                 // Get all tracks from posts
+                requests.push(db.collection('tracks').doc(post.trackId));
+            })
+            return Promise.all(requests);
+        }).then(docs => {
+            let tracks = [];
+            for (let i = 0; i < data.length; i++) {
+                let data = docs[i].data();      // Create tracks
+                tracks.push(
+                    createTrack(
+                        data.trackId,
+                        posts[i]["postedBy"],
+                        data.userId,
+                        getElapsedTime(data.uploadDate),
+                        data.audio,
+                        props.isPlaying,
+                        props.togglePlay,
+                        data.trackName,
+                        data.playCount,
+                        data.likes,
+                        data.commentCount,
+                        data.repostCount,
+                        data.trackArt,
+                    )
+                )
+            }
+            setTracks(tracks);      // Push tracks to state
         })
     }
+
+    useEffect(() => {
+        getPosts();
+    }, [])
 
     return (
         <>
             <h2>
                 Here are the latest posts from the artists you follow:
-        </h2>
-
+            </h2>
+            {tracks}
         </>
     )
 }
