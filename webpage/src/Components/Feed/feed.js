74bdcd94bf6a5dc735fbe4976d68c74aa801445f
apparatusjazz <maui.arcuri@gmail.com>
2020-11-21 12:50:29 -0500

    Fixed errors in data for getting posts

--- webpage/src/Components/Feed/feed.js
+++ webpage/src/Components/Feed/feed.js
@@ -11,38 +11,39 @@ const Feed = (props) => {
 
     const getPosts = () => {
         let posts = [], following = [], requests = [];
-        db.collection('users').doc(user.uid).get().then(doc => {
+        db.collection('users-1').doc('user_0').get().then(doc => {
             const data = doc.data();
             following = [...data.following];
         }).then(() => {
             following.forEach(artist => {
-                requests.push(db.collection('users').doc(artist).get());
-            });
-            return Promise.all(requests);           // Get all users followed
+                requests.push(db.collection('users-1').doc(artist).get());
+            })
+            return Promise.all(requests);
         }).then(docs => {
-            for (let i = 0; i < data.length; i++) {
-                let data = docs[i].data();
-                let newPosts = data.posts;
+            let items = docs.map(doc => doc.data());
+            for (let i = 0; i < items.length; i++) {
+                // let data = items[i];
+                let newPosts = items[i].posts;
                 newPosts.forEach(post => {          // Get all posts by user and add the userName to the post object
-                    post["postedBy"] = data.displayName;
+                    post["postedBy"] = items[i].displayName;
                 })
-                posts.concat(data.posts);
+                posts = posts.concat(newPosts);
             }
             posts.sort((a, b) => b.postDate - a.postDate);  // Sort by latest posts first
             requests = [];
-
             // Get all tracks from posts
             for (let i = 0; i < posts.length || i < 25; i++) { // Hard limit on posts shown, this can be changed in future
-                requests.push(db.collection('tracks').doc(posts[i].trackId));
+                requests.push(db.collection('tracks-1').doc(posts[i].trackId).get());
             }
             return Promise.all(requests);
         }).then(docs => {
             let tracks = [];
-            for (let i = 0; i < data.length; i++) {
-                let data = docs[i].data();      // Create tracks
+            let items = docs.map(doc => doc.data());
+            for (let i = 0; i < items.length; i++) {
+                let data = items[i];      // Create tracks
                 tracks.push(
                     createTrack(
-                        data.trackId,
+                        `${data.trackId}_inst_${i}`,
                         posts[i]["postedBy"],
                         data.userId,
                         getElapsedTime(data.uploadDate),
@@ -51,7 +52,7 @@ const Feed = (props) => {
                         props.togglePlay,
                         data.trackName,
                         data.playCount,
-                        data.likes,
+                        data.likeCount,
                         data.commentCount,
                         data.repostCount,
                         data.trackArt,
