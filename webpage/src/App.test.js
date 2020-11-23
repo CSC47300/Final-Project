b666b4f8d3711896bd6ad0657a0882a6b96d28f6
apparatusjazz <maui.arcuri@gmail.com>
2020-11-23 12:59:04 -0500

    Update elapsed time test

--- webpage/src/App.test.js
+++ webpage/src/App.test.js
@@ -9,7 +9,7 @@ import { getElapsedTime } from './Helpers/helpers';
 //   expect(linkElement).toBeInTheDocument();
 // });
 test('get elapsed time in string format', () => {
-    expect(getElapsedTime(Date.now())).toBe("0 seconds ago");
-    expect(getElapsedTime(Date.now() - 10000)).toBe("10 seconds ago");
-    expect(getElapsedTime(Date.now() - 70000)).toBe("1 minute ago");
+    expect(getElapsedTime(Date.now())).toBe("0 seconds");
+    expect(getElapsedTime(Date.now() - 10000)).toBe("10 seconds");
+    expect(getElapsedTime(Date.now() - 70000)).toBe("1 minute");
 });
